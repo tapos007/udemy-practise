7c4eba1d9d09c78ba7a3c4dfedb88b0b3388cc4a
Biswa Nath Ghosh <tapos.aa@gmail.com>
2021-02-12 19:31:45 +0600

    final commit done

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -14,7 +14,7 @@ namespace API
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-                 }
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
